a02347d141fdd5c7c8abef8cfd82e695b78db831
mbp@fcs1250 <buiphuocminh94@gmail.com>
2026-06-18 17:50:54 +0700

    feat(markdown): configure img-clip.nvim for note assets

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,10 @@ local utils = require("minh.utils")
 
 vim.keymap.set("n", "<leader>wr", utils.hardwrap_md, { desc = "Hard Wrap 80" })
 vim.keymap.set("n", "<leader>tc", utils.markdown_toc, { desc = "Table of Content" })
+
+require('img-clip').setup({
+    default = {
+        dir_path = vim.g.note_path .. 'assets/images',
+        use_absolute_path = true
+    }
+})
