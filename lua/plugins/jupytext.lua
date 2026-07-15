43910590617547b3c3f43fa72a78c899bda27d8d
mbp@fcs1250 <buiphuocminh94@gmail.com>
2026-07-15 15:07:45 +0700

    feat(jupytext): sync only when counterpart file exists

--- lua/plugins/jupytext.lua
+++ lua/plugins/jupytext.lua
@@ -12,21 +12,31 @@ return {
 			group = jupytext_sync,
 			pattern = { "*.ipynb", "*.md" },
 			callback = function()
-				local current_file = vim.fn.expand("%")
-				vim.fn.jobstart({
-					"jupytext",
-					"--set-formats",
-					"ipynb,md",
-					"--sync",
-					current_file,
-				}, {
-					on_exit = function(_, code)
-						if code == 0 then
-						else
-							vim.notify("Jupytext: Sync Error!", vim.log.levels.ERROR)
-						end
-					end,
-				})
+				local current_file = vim.fn.expand("%:p")
+				local extension = vim.fn.expand("%:e")
+				local target_file
+
+				if extension == "md" then
+					target_file = vim.fn.fnamemodify(current_file, ":r") .. ".ipynb"
+				elseif extension == "ipynb" then
+					target_file = vim.fn.fnamemodify(current_file, ":r") .. ".md"
+				end
+
+				if target_file and vim.fn.filereadable(target_file) == 1 then
+					vim.fn.jobstart({
+						"jupytext",
+						"--set-formats",
+						"ipynb,md",
+						"--sync",
+						current_file,
+					}, {
+						on_exit = function(_, code)
+							if code ~= 0 then
+								vim.notify("Jupytext: Sync Error!", vim.log.levels.ERROR)
+							end
+						end,
+					})
+				end
 			end,
 		})
 	end,
